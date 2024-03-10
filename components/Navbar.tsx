840a1dafeb22c4ab42a4f604d951f216b55e9b27
pinnng29 <apingspeedholic@gmail.com>
2024-03-10 17:30:30 +0700

    fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,18 +7,20 @@ import ContactButton from './contact-button';
 
 export default function Navbar() {
     return (
-        <nav className="flex h-[64px] items-center justify-between border-b border-border px-4 shadow-md">
-            <Logo />
-            <NavigationBar />
-            <div className="flex items-center gap-3">
-                <ContactButton />
-                <Avatar>
-                    <AvatarImage
+        <div className="flex flex-col bg-background">
+            <nav className="flex items-center justify-between border-border border-b h-[64px] px-6 py-2 shadow-md">
+                    <Logo />
+                    <NavigationBar />
+                <div className="flex items-center gap-x-3">
+                    <ContactButton />
+                    <Avatar>
+                        <AvatarImage
                         src="https://github.com/shadcn.png"
                         alt="@shadcn"
-                    />
-                </Avatar>
-            </div>
-        </nav>
+                        />
+                    </Avatar>
+                </div>
+            </nav>
+        </div>
     );
 }
